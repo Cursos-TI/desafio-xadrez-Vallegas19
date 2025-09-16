a216e84a951072aaf517d0a4374ccd49bebde93a
Vallegas19 <biiel.vallegas14@gmail.com>
2025-09-16 15:40:48 +0000

    Terminei de adicionar as cartas

--- xadrez.c
+++ xadrez.c
@@ -2,21 +2,17 @@
 
 
 int main(){
-    printf("Desafio Xadrez!\n");
-
     //Carta 1
-    printf("Carta1\n");
 
     char Estado1;         
     char Codigo1;         
-    char NomeDaCidade1[20];
+    char NomeDaCidade1[50];
     int Populacao1; 
     float Area1; 
     float PIB1;
     int NumerodePontosTurísticos1;
 
     //Carta2
-    printf("Carta2\n");
 
     char Estado2;         
     char Codigo2;
@@ -27,9 +23,8 @@ int main(){
     int NumerodePontosTurísticos2;
 
 
-    //Dados da carta1
+    //Dados da Carta 1
     printf("Carta1\n");
-
     printf("Digite seu Estado: \n");
     scanf("%c", &Estado1);
 
@@ -40,39 +35,60 @@ int main(){
     scanf("%s", &NomeDaCidade1);
 
     printf("Digite a população: \n");
-    scanf("%d", Populacao1);
+    scanf("%d", &Populacao1);
 
     printf("Digite a Área: \n");
-    scanf("%f", Area1);
+    scanf("%f", &Area1);
 
     printf("Digite o PIB \n");
-    scanf("%f", PIB1);
+    scanf("%f", &PIB1);
 
     printf("Digite o numero de pontos turisticos: \n");
     scanf("%d", &NumerodePontosTurísticos1);
 
-    //Carta 2
-    printf("Carta 1\n");
-
+    //Dados da Carta 2
+    printf("Carta 2\n");
     printf("Digite seu Estado: \n");
     scanf("%c", &Estado2);
 
     printf("Digite seu Codigo: \n");
-    scanf("%s", Codigo2);
+    scanf("%s", &Codigo2);
 
     printf("Digite o nome da sua Cidade: \n");
     scanf("%s", &NomeDaCidade2);
 
     printf("Digite a população: \n");
-    scanf("%d", populacao2);
+    scanf("%d", &populacao2);
 
     printf("Digite a Área: \n");
-    scanf("%f", Area2);
+    scanf("%f", &Area2);
 
     printf("Digite o PIB: \n");
     scanf("%f", &PIB2);
 
     printf("Digite o número de pontos Turisticos: \n");
-    scanf("%d", NumerodePontosTurísticos2);
+    scanf("%d", &NumerodePontosTurísticos2);
 
+    //Exibição dos dados
+    printf("\n-----Dados das Cartas Cadastradas-----\n");
+
+    printf("\n Dados da Carta 1: \n");
+    printf("Estado: %c\n", Estado1);
+    printf("Código: %s\n", Codigo1);
+    printf("Nome da cidade: %s\n", NomeDaCidade1);
+    printf("População: %d\n", Populacao1);
+    printf("Área: %f km²\n");
+    printf("PIB: %f", PIB1);
+    printf("Número de Pontos Turisticos: %d\n", NumerodePontosTurísticos1);
+
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", Estado2);
+    printf("Código: %s\n", Codigo2);
+    printf("Nome da cidade: %s\n", NomeDaCidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %f km²\n");
+    printf("PIB: %f\n", PIB2);
+    printf("Número de Pontos Turisticos: %d\n", NumerodePontosTurísticos2);
+
+return 0;
 }
